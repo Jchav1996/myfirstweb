f4dc22abee779770a910d0cbc67850a28e34d43a
jchav1996 <jenniferschavarria@gmail.com>
2023-11-03 17:01:24 -0400

    changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,7 +7,7 @@ export function Hero() {
   <div className="column">
     <a target="_blank" href="https://youtube.com/pinkfong"><img src="https://static.wixstatic.com/media/ca2d44_8b6a95a84e5e4ff0b30496104569b948~mv2.png/v1/fill/w_602,h_496,al_c,lg_1,q_85,enc_auto/Rev_Home.png"
      /></a>
-    <p>Pinkfong videos</p>
+    <p><b>Pinkfong videos</b></p>
   </div>
   <div className="column2">
     <a target="_blank" href="https://www.youtube.com/watch?v=XOnHtStmbCI&list=PLF08D582EDFB922EE"> <img src="https://www.pngitem.com/pimgs/m/115-1152980_mickey-mouse-clubhouse-png-mickey-mouse-and-friends.png" 
@@ -17,7 +17,7 @@ export function Hero() {
   <div className="column3">
     <a target="_blank" href="https://youtube.com/watch?v=b36HSGq7914"> <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/40c2cd1a-2c65-4b21-81e1-31ee5f01abc9/dfmcu18-027c7719-7bda-4570-85b8-87cef964f8a2.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQwYzJjZDFhLTJjNjUtNGIyMS04MWUxLTMxZWU1ZjAxYWJjOVwvZGZtY3UxOC0wMjdjNzcxOS03YmRhLTQ1NzAtODViOC04N2NlZjk2NGY4YTIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.2fopdiQpaoKpr6CtPVaeFP5g3ZmZl4bFKci0tjsc83c" 
     /></a>
-    <p>Spidey and his amazing friends: LIVE Episodes</p>
+    <p><b>Spidey and his amazing friends: LIVE Episodes</b></p>
     </div>
         </div>
     );
