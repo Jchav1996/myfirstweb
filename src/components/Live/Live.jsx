dc247b2f682ca696a5b287747866180ecd9140ce
jchav1996 <jenniferschavarria@gmail.com>
2023-11-06 19:19:40 -0500

    changes

--- src/components/Live/Live.jsx
+++ src/components/Live/Live.jsx
@@ -3,9 +3,9 @@ import './live.css'
 export function Live () {
     return (
 
-        <div class="video">
+        <div className="video">
         <h1 id='home'>Watch Cocomelon LIVE</h1>
-        <iframe width="600" height="335" src="https://www.youtube.com/embed/1hE34xA_pRc?si=8kS2ggIgbdEbwUyu&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="600" height="335" src="https://www.youtube.com/embed/1hE34xA_pRc?si=8kS2ggIgbdEbwUyu&amp;controls=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         <br></br>
  </div>
     );
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -3,15 +3,15 @@ import './navbar.css'
 export function Navbar () {
     return (
     <nav>
-        <div class="navbar">
+        <div className="navbar">
         <h2> KidzWorlz</h2>
   <a href="#home">Live</a>
   <a href="#smLink">Social</a>
-  <div class="dropdown">
-    <button class="dropbtn">Shop 
-      <i class="fa fa-caret-down"></i>
+  <div className="dropdown">
+    <button className="dropbtn">Shop 
+      <i className="fa fa-caret-down"></i>
     </button>
-    <div class="dropdown-content">
+    <div className="dropdown-content">
       <a target='_blank' href="https://www.amazon.com/stores/page/BA2B692D-8665-434C-9F1C-CBB20CAED180?channel=OfficialWeb">Pinkfong</a>
       <a target='_blank' href="https://www.shopdisney.com/characters/disney/mickey-mouse/">Mickey Mouse</a>
       <a target='_blank' href="https://www.shopdisney.com/movies-shows/disney/disney-junior/spidey-and-his-amazing-friends/">Spidey</a>
